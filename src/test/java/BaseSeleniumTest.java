d9698a23a789ba12b4d4ab490cab9fd31259a308
mackowit <mackowit@o2.pl>
2021-01-24 11:45:34 +0100

    init

---
+++ src/test/java/BaseSeleniumTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class BaseSeleniumTest {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+    }
+
+    @AfterTest
+    public void cleanUp() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class SeleniumTest extends BaseSeleniumTest {
+
+        @Test
+        public void localWebSiteTest() {
+            driver.get("C:\\Users\\Samsung\\Downloads\\Test.html");
+    }
+}
